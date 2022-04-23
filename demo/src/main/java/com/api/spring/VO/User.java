b56ecd2e5c83c509eb7fe689c110c7f5a8847c0f
chokim-01 <chokim159@naver.com>
2022-04-24 02:51:17 +0900

    mysql sync and dml Test

--- demo/src/main/java/com/api/spring/VO/User.java
+++ demo/src/main/java/com/api/spring/VO/User.java
@@ -1,6 +1,28 @@
 package com.api.spring.VO;
 
+import lombok.*;
 
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user")
+@Builder
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 30, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 10, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
 
 }
